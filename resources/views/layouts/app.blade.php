630083e48e94722fb0e2a5113801420a4e2367b5
Andrew Jensen <navysealboy26@hotmail.com>
2020-06-29 17:15:14 -0600

    add in cdn for fontawesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
 
         <title>{{ env('APP_NAME') }}</title>
 
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body class="bg-black-alt font-sans leading-normal tracking-normal">
@@ -14,7 +15,7 @@
             <div class="w-full container mx-auto flex flex-wrap items-center mt-0 pt-3 pb-3 md:pb-0">
 
                 <div class="w-1/2 pl-2 md:pl-0">
-                    <a class="text-gray-100 text-base xl:text-xl no-underline hover:no-underline font-bold"  href="{{ route('home') }}">
+                    <a class="text-gray-100 text-base xl:text-xl no-underline hover:no-underline font-bold"  href="#">
                         {{ env('APP_NAME') }}
                     </a>
                 </div>
