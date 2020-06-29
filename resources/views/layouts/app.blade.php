15da098058078cfb5a8ce7d2f7dfdd6ab339b6f7
Andrew Jensen <navysealboy26@hotmail.com>
2020-06-29 16:41:45 -0600

    add sweetalerts2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,24 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>App Name - @yield('title')</title>
+        <title>Time Tracker</title>
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Test</a>
+                </li>
+                <li>
+                    <a href="#">Tester</a>
+                </li>
+                <li>
+                    <a href="#">Testered</a>
+                </li>
+            </ul>
+        </nav>
         <div class="container mx-auto px-4">
             @yield('content')
         </div>
