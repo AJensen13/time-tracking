9e39fd3bba66bb74b171ee909dd1226cbe8555d9
Andrew Jensen <navysealboy26@hotmail.com>
2020-06-29 15:08:13 -0600

    set up tailwind

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>App Name - @yield('title')</title>
+
+        <link href="{{ mix('/css/app.css') }}">
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
