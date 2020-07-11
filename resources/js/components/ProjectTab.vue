ee673c000b45dde89610ebf764177d9d2cb3f3c6
Andrew Jensen <navysealboy26@hotmail.com>
2020-07-11 12:12:02 -0600

    add components

---
+++ resources/js/components/ProjectTab.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="w-full md:w-1/2 xl:w-1/3 p-3">
+        <div class="bg-black border border-grey-darkest rounded shadow p-2">
+            <div class="flex flex-row items-center">
+                <div class="flex-1 text-right md:text-center">
+                    <h5 class="uppercase text-white">project.name</h5>
+                    <h3 class="text-3xl text-grey-dark">project.taskCount Active Tasks</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
